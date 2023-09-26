573bd9adb50845581e275ddbcd2cb2fd54e295b3
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-09-26 13:27:44 +0200

    feat: create 'createGnssObject' method
    give the oportunity to document decisions. Also, remove the noise of have a 'big' object definition in getGnss

--- src/utils/getGnss.ts
+++ src/utils/getGnss.ts
@@ -30,24 +30,11 @@ export const getGnss = (location?: Location_6): GetGnssResult => {
 			}),
 		}
 
-	const { 0: lat, 2: alt, 6: spd, 1: lng, 3: acc } = location
+	const { [0]: lat, [2]: alt, [6]: spd, [1]: lng, [3]: acc } = location
+
 	const time = getTime(location)
 
-	/**
-	 * hdg from GNSS object is not provided.
-	 *
-	 * @see https://github.com/MLopezJ/asset-tracker-lwm2m-js/blob/saga/adr/009-nrf-asset-tracker-values-not-provided.md
-	 */
-	const object = {
-		v: {
-			lng,
-			lat,
-			acc,
-			alt,
-			spd,
-		},
-		ts: time,
-	}
+	const object = createGnssObject({ lat, lng, acc, alt, spd, time })
 
 	return validateAgainstSchema(object, GNSS)
 }
@@ -57,3 +44,37 @@ export const getGnss = (location?: Location_6): GetGnssResult => {
  * Value is in seconds and it is multiplied to transform to milliseconds.
  */
 const getTime = (location: Location_6): number => location['5'] * 1000
+
+/**
+ * Creates GNSS object defined by 'nRF Asset Tracker Reported'.
+ * @see {@link documents/gnss.md}
+ *
+ * 'hdg' key from 'GNSS' object defined by 'nRF Asset Tracker Reported' is not provided.
+ * @see https://github.com/MLopezJ/asset-tracker-lwm2m-js/blob/saga/adr/009-nrf-asset-tracker-values-not-provided.md
+ */
+const createGnssObject = ({
+	lat,
+	lng,
+	acc,
+	alt,
+	spd,
+	time,
+}: {
+	lat: number
+	lng: number
+	acc: number | undefined
+	alt: number | undefined
+	spd: number | undefined
+	time: number
+}) => {
+	return {
+		v: {
+			lng,
+			lat,
+			acc,
+			alt,
+			spd,
+		},
+		ts: time,
+	}
+}
