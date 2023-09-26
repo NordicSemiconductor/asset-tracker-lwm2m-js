4566945256776e779ea6f2220f7a717800005f89
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-09-26 15:28:15 +0200

    docs: remove and add empty lines

--- src/utils/getBat.ts
+++ src/utils/getBat.ts
@@ -17,9 +17,9 @@ type GetBatResult =
 	| { result: BatteryData }
 	| { error: ValidationError }
 	| { warning: UndefinedLwM2MObjectWarning }
+
 /**
  * Takes object id 3 (device) from 'LwM2M Asset Tracker v2' and convert into 'bat' object from 'nRF Asset Tracker Reported'
- *
  * @see https://github.com/MLopezJ/asset-tracker-cloud-coiote-azure-converter-js/blob/saga/documents/battery.md
  */
 export const getBat = (device?: Device_3): GetBatResult => {
